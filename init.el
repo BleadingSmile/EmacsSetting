e715101a682ebae895a8cbf279eae2827f6ae58e
Larry Hsiao <larryhsiao@silverhetch.com>
2018-03-13 01:32:00 +0000

    update for normal purpose

--- init.el
+++ init.el
@@ -2,41 +2,13 @@
 (add-to-list 'package-archives
 	     '("melpa" . "http://melpa.org/packages/"))
 (package-initialize)
+(require 'use-package)
 
 (require 'company)
-(require 'company-irony)
-(require 'company-irony-c-headers)
-(require 'company-rtags)
 (global-company-mode)
-(add-hook 'irony-mode-hook
-	  'company-irony-setup-begin-commands)
-(setq company-backends
-      (delete 'company-semantic company-backends))
-(eval-after-load 'company
-  '(add-to-list
-    'company-backends
-    '(company-irony-c-headers company-irony)))
-(setq company-idle-delay 0)
-(define-key c-mode-map [(tag)] 'company-complete)
-(define-key c++-mode-map [(tab)] 'company-complete)
-
-(require 'flycheck)
-(require 'flycheck-rtags)
-(require 'flycheck-irony)
-(add-hook 'c++-mode-hook 'flycheck-mode)
-(add-hook 'c-mode-hook 'flycheck-mode)
-(defun my-flycheck-rtags-setup ()
-  (flycheck-select-checker 'rtags)
-  (setq-local flycheck-highlighting-mode nil)
-  (setq-local flycheck-check-syntax-automatically nil))
-(add-hook 'c-mode-common-hook #'my-flycheck-rtags-setup)
-(eval-after-load 'flycheck
-  '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
 
 (require 'helm)
 (require 'helm-config)
-(require 'helm-rtags)
-(require 'helm-c-yasnippet)
 (helm-mode 1)
 (global-set-key (kbd "C-c h") 'helm-command-prefix)
 (global-unset-key (kbd "C-x c"))
@@ -48,20 +20,9 @@
 (setq helm-split-window-in-side-p t
       helm-move-to-line-cycle-in-source t
       helm-ff-search-library-in-sexp t
-      helm-scroll-amount 8 ;scroll lines other window using m-<next>/m-<prior>
+      helm-scroll-amount 8
       helm-ff-file-name-history-use-recentf t
       helm-echo-input-in-header-line t)
-(defun spacemacs//helm-hide-minibuffer-maybe ()
-  "Hide minibuffer in Helm session if we use the header line as input feild."
-  (when (with-helm-buffer helm-echo-input-in-header-line)
-    (let ((ov (make-overlay (point-min) (point-max) nil nil t)))
-      (overlay-put ov 'window (selected-window))
-      (overlay-put ov 'face
-		   (let ((bg-color (face-background 'default nil)))
-		     '(:background ,bg-color :foreground ,bg-color)))
-      (setq-local cursor-type nil))))
-(add-hook 'helm-minibuffer-set-up-hook
-	  'spacemacs//helm-hide-minibuffer-maybe)
 (helm-autoresize-mode 1)
 (setq helm-autoresize-max-height 0)
 (setq helm-autoresize-min-height 50)
@@ -69,10 +30,9 @@
 (setq helm-buffers-fuzzy-matching t
       helm-recentf-fuzzy-match t)
 (setq helm-locate-fuzzy-match t)
-(setq helm-apropos-fuzzy-match t)
 (setq helm-lisp-fuzzy-completion t)
 (global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "M-y") "helm-show-kill-ring")
+(global-set-key (kbd "M-y") "helm-show-killl")
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 (when (executable-find "ack-grep")
   (setq helm-grep-default-command "ack-grep -Hn --no-group --no-color %e %p %f"
@@ -84,11 +44,12 @@
 (global-set-key (kbd "C-c h g") 'helm-google-suggest)
 (global-set-key (kbd "C-c h M-:") 'helm-eval-expression-with-eldoc)
 
+(require 'shell)
 (require 'helm-eshell)
 (add-hook 'eshell-mode-hook
 	  #'(lambda ()
 	      (define-key eshell-mode-map (kbd "C-c C-l") 'helm-eshell-history)))
-(define-key shell-mode-map (kbd "C-c c-l") 'helm-comint-input-ring)
+(define-key shell-mode-map (kbd "C-c C-l") 'helm-minibuffer-history)
 (define-key minibuffer-local-map (kbd "C-c C-l") 'helm-minibuffer-history)
 
 (require 'helm-projectile)
@@ -106,57 +67,27 @@
 (global-set-key (kbd "<f6>") 'helm-ls-git-ls)
 (global-set-key (kbd "C-x C-d") 'helm-browse-project)
 
-(require 'nyan-mode)
-(nyan-mode)
-
-(require 'zone-select)
-(require 'zone-nyan)
-(require 'zone-sl)
-(require 'zone-rainbow)
-(zone-select-add-program 'zone-nyan)
-(zone-select-add-program 'zone-sl)
-(zone-select-add-program 'zone-rainbow)
-
-(require 'yasnippet)
-(yas-global-mode 1)
-
-(require 'rtags)
-(setq rtags-use-helm t)
-(setq rtags-completions-enabled t)
-(eval-after-load 'company
-  '(add-to-list
-    'company-backends 'company-rtags))
-(setq rtags-autostart-diagnostics t)
-(rtags-enable-standard-keybindings)
-
-(require 'irony)
-(add-hook 'c++-mode-hook 'irony-mode)
-(add-hook 'c-mode-hook 'irony-mode)
-(defun my-irony-mode-hook()
-  (define-key irony-mode-map [remap completion-at-point]
-    'irony-completion-at-point-async)
-  (define-key irony-mode-map [remap complete-symbol]
-    'irony-completion-at-point-async))
-(add-hook 'irony-mode-hook 'my-irony-mode-hook)
-(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
-
-(require 'cmake-ide)
-(cmake-ide-setup)
-(setq cmake-ide-flags-c++ (append '("-std=c++11")))
-(global-set-key (kbd "C-c m") 'cmake-ide-compile)
-
 (require 'autopair)
 (autopair-global-mode)
 
-(require 'srefactor)
-(require 'srefactor-lisp)
-(semantic-mode 1)
-(define-key c-mode-map (kbd "M-RET") 'srefactor-refactor-at-point)
-(define-key c++-mode-map (kbd "M-RET") 'srefactor-refactor-at-point)
-(setq srefactor-ui-menu-show-help nil)
+(require 'nyan-mode)
+(nyan-mode)
+
+(require 'magit)
+(global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
 
 (require 'clean-aindent-mode)
 (add-hook 'prog-mode-hook 'clean-aindent-mode)
+
+(require 'markdown-mode)
+(use-package markdown-mode
+	     :ensure t
+	     :commands (markdown-mode gfm-mode)
+	     :mode (("README\\.md\\'" . gfm-mode)
+		    ("\\.md\\'" . markdown-mode)
+		    ("\\.markdown\\'" . markdown-mode))
+	     :init (setq markdown-command "multimarkdown"))
 
 (require 'sr-speedbar)
 (setq sr-speedbar-skip-other-window-p t)
@@ -172,19 +103,14 @@
   (add-hook 'speedbar-before-visiting-file-hook 'select-next-window t)
   (add-hook 'speedbar-before-visiting-tag-hook 'select-next-window t))
 
-(require 'magit)
-(global-set-key (kbd "C-x g") 'magit-status)
-(global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
-
-;;built-in settings
+;;built-in functions
 (hl-line-mode)
 (global-linum-mode t)
 (global-set-key (kbd "RET") 'newline-and-indent)
 (global-set-key (kbd "C-c s") 'select-line)
 (add-hook 'prog-mode-hook
 	  (lambda () (interactive)
-	    (setq show-trailing-whitespace 1)))
-(setq c-default-style "ellemtel")
+	        (setq show-trailing-whitespace 1)))
 (setq speedbar-show-unknown-files t)
 
 (defun select-current-line()
@@ -202,17 +128,11 @@
 	(end-of-line))
     (select-current-line)))
 
-;not used currently
-(defun cut-line-or-region()
-  "Cut current line, or test selection."
-  (interactive)
-  (if current-prefix-arg
-      (progn
-	(kill-new (buffer-string))
-	(delete-region (point-min) (point-max)))
-    (progn (if (use-region-p)
-	       (kill-region (region-beginning) (region-end) t)
-	     (kill-region (line-beginning-position) (line-beginning-position 2))))))
+(global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
+(global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
+(global-set-key (kbd "S-C-<down>") 'shrink-window)
+(global-set-key (kbd "S-C-<up>") 'enlarge-window)
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -220,14 +140,37 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
+   [default bold shadow italic underline bold bold-italic bold])
  '(ansi-color-names-vector
-   ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
- '(custom-enabled-themes (quote (suscolors)))
+   (vector "#212121" "#B71C1C" "#558b2f" "#FFA000" "#2196f3" "#4527A0" "#00796b" "#FAFAFA"))
+ '(custom-enabled-themes (quote (zenburn)))
  '(custom-safe-themes
    (quote
-    ("b9b1a8d2ec1d5c17700e1a09256f33c2520b26f49980ed9e217e444c381279a9" "d606ac41cdd7054841941455c0151c54f8bff7e4e050255dbd4ae4d60ab640c1" default)))
- '(safe-local-variable-values (quote ((cmake-ide-build-dir . build)))))
+    ("98cc377af705c0f2133bb6d340bf0becd08944a588804ee655809da5d8140de6" "e11569fd7e31321a33358ee4b232c2d3cf05caccd90f896e1df6cab228191109" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "ff7625ad8aa2615eae96d6b4469fcc7d3d20b2e1ebc63b761a349bebbb9d23cb" "5dc0ae2d193460de979a463b907b4b2c6d2c9c4657b2e9e66b8898d2592e3de5" "66881e95c0eda61d34aa7f08ebacf03319d37fe202d68ecf6a1dbfd49d664bc3" default)))
+ '(fci-rule-color "#dadada")
+ '(hl-sexp-background-color "#efebe9")
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#B71C1C")
+     (40 . "#FF5722")
+     (60 . "#FFA000")
+     (80 . "#558b2f")
+     (100 . "#00796b")
+     (120 . "#2196f3")
+     (140 . "#4527A0")
+     (160 . "#B71C1C")
+     (180 . "#FF5722")
+     (200 . "#FFA000")
+     (220 . "#558b2f")
+     (240 . "#00796b")
+     (260 . "#2196f3")
+     (280 . "#4527A0")
+     (300 . "#B71C1C")
+     (320 . "#FF5722")
+     (340 . "#FFA000")
+     (360 . "#558b2f"))))
+ '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
